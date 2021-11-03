624368516715d024acbce97d7bec74da1e292c49
Saikumar <saikumar.patnaikuni@kelltontech.com>
2021-11-03 14:13:30 +0530

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'coding-assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coding-assignment');
+    expect(app.title).toEqual('coding-assignment..');
   });
 
   it('should render title', () => {
