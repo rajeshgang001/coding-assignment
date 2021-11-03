d7d1cba19c200fc5d60e54661f300ef2ccb0358d
Rajesh <rajeshgang001@gmail.com>
2021-11-03 15:35:14 +0530

    recent changes

--- src/app/components/anagram-tester/anagram-tester.component.ts
+++ src/app/components/anagram-tester/anagram-tester.component.ts
@@ -8,21 +8,14 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AnagramTesterComponent implements OnInit {
-  error: string = null;
 
   constructor() { }
 
   ngOnInit(): void { }
 
   onCheckAnagram(form: NgForm) {
-    if (form.value.firstWord.length == form.value.secondWord.length) {
-      this.error = null;
       if (form.value.firstWord.split("").sort().join("") === form.value.secondWord.split("").sort().join(""))
         alert('Both words are anagrams');
       else alert('Both words are NOT anagrams');
-    }
-    else {
-      this.error = "Both words are not equal in lengths";
-    }
   }
 }
